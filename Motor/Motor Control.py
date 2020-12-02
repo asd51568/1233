64482158cb2581bf317e5657d7b6b2d60cc86d8b
kimhyuntae <73161035+Kiimhyuntae@users.noreply.github.com>
2020-12-02 19:12:18 +0900

    Update Motor Control.py

--- Motor/Motor Control.py
+++ Motor/Motor Control.py
@@ -1,61 +1,4 @@
-import RPi.GPIO as GPIO 
-from time import sleep  
-
-servoPin          = 12   
-SERVO_MAX_DUTY    = 12   
-SERVO_MIN_DUTY    = 3   
-
-GPIO.setmode(GPIO.BOARD)        
-GPIO.setup(servoPin, GPIO.OUT)  
-
-servo = GPIO.PWM(servoPin, 50)  
-servo.start(0)  
-'''
-서보 위치 제어 함수
-degree에 각도를 입력하면 duty로 변환후 서보 제어(ChangeDutyCycle)
-'''
-def setServoPos(degree):
-  # 각도는 180도를 넘을 수 없다.
-  if degree > 180:
-    degree = 180
-
-  # 각도를 duty로 변경한다.
-  duty = SERVO_MIN_DUTY+(degree*(SERVO_MAX_DUTY-SERVO_MIN_DUTY)/180.0)
-  # duty 값 출력
-  print("Degree: {} to {}(Duty)".format(degree, duty))
-
-  # 변경된 duty값을 서보 pwm에 적용
-  servo.ChangeDutyCycle(duty)
-
-  #서보 작동 테스트용 코드
-def main()
-  # 서보 0도
-  setServoPos(0)
-  sleep(1) # 1초 대기
-  
-  # 90도
-  setServoPos(90)
-  sleep(1)
-  
-  # 50도
-  setServoPos(50)
-  sleep(1)
-
-  # 120도
-  setServoPos(120)
-  sleep(1)
-
-  # 180도
-  setServoPos(180)
-  sleep(1)
-
-  servo.stop()
-  GPIO.cleanup()
-  
-if __name__ == "__main__":  
-  main()
-  
-  #실제 사용할 예정인 코드
+#실제 사용할 예정인 코드
   
 import RPi.GPIO as GPIO 
 from time import sleep  
@@ -143,3 +86,35 @@ servo1.ChangeDutyCycle(0)
 servo1.stop()
 GPIO.cleanup()
 print ("Goodbye")
+
+#2
+#Import libraries
+import RPi.GPIO as GPIO
+import time
+
+#Set GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
+
+#Set pin 11 as av output, and set servo1 as pin 11 as PWM
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) #Note 11 is pin, 50 = 50Hz pulse
+
+#start PWM running, but with value of 0 (pulse off)
+servo1 start(0)
+
+#Loop to allow user to set servo angle. Try/finally allows exit
+#with execution of servo. stop and GPIO cleanup
+
+try:
+  while True:
+    #Ask user for angle and turn servo to it
+    angle = float(input('Enter angle between 0 & 180: '))
+    servo1.ChangeDutyCycle(2+(angle/18))
+    time.sleep(0.5)
+    servo1.ChangeDutyCycle(0)
+    
+finally:
+  #Clean things up at the end
+  servo1.stop()
+  GPIO.cleanup()
+  print("Goodbye!")
