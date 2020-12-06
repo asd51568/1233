5bd97ae938982269499f0d25cd572b27fe125e76
kimhyuntae <73161035+Kiimhyuntae@users.noreply.github.com>
2020-12-07 01:59:12 +0900

    Update Use.py

--- Motor/Use.py
+++ Motor/Use.py
@@ -11,11 +11,7 @@ servo1 = GPIO.PWM(11,50) #Note 11 is pin, 50 = 50Hz pulse
 
 #start PWM running, but with value of 0 (pulse off)
 servo1 start(0)
-print ("waiting for 2 seconds")
-time.sleep(2)
-
-#Let's move the servo!
-print ("Rotating 180 degrees in 10 steps")
+time.sleep(1)
 
 #Define variable duty
 duty = 2
