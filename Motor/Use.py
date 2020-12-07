2a6229434050a851235f9c61160051d9878bae5b
kimhyuntae <73161035+Kiimhyuntae@users.noreply.github.com>
2020-12-07 21:07:25 +0900

    Update Use.py

--- Motor/Use.py
+++ Motor/Use.py
@@ -5,33 +5,69 @@ import time
 #Set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 
+#Set pin 16 as TRIG and set pin 18 as ECHO
+TRIG = 16
+ECHO = 18
+
+
+print ("Distance Measurement In Progress")
+
+#Set TRIG as output and set ECHO as input
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 #Set pin 11 as av output, and set servo1 as pin 11 as PWM
 GPIO.setup(11,GPIO.OUT)
 servo1 = GPIO.PWM(11,50) #Note 11 is pin, 50 = 50Hz pulse
-
-#start PWM running, but with value of 0 (pulse off)
-servo1 start(0)
-time.sleep(1)
-
-#Define variable duty
-duty = 2
-
-#Loop for duty values from 2 to 12 (0 to 180 degrees)
-while duty <= 12:
-  servo1.ChangeDutyCycle(duty)
-  time.sleep(0.5)
-  duty = duty + 1
-  
-#Wait a couple of seconds
-time.sleep(5)
-
-#Turn back to 0 degrees
-print ("Turning back to 0 degrees")
-servo1.ChangeDutyCycle(2)
-time.sleep(0.5)
-servo1.ChangeDutyCycle(0)
-
-#Clean things up at the end
-servo1.stop()
-GPIO.cleanup()
-print ("Goodbye")
+ 
+try:
+  #start PWM running, but with value of 0 (pulse off)
+    servo1 start(0)
+    while True:
+        
+        GPIO.output(TRIG, False)
+        time.sleep(0.5)
+        
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        
+        while GPIO.input(ECHO)==0:
+            pulse_start = time.time()
+        
+        while GPIO.input(ECHO)==1:
+            pulse_end = time.time()
+        
+        #Calculating Distance
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        #Print distance
+        print ("Distance: ",distance,"cm")
+        
+        #거리에 맞게 서보모터를작동시키기
+        if distance > 20:
+            servo1.ChangeDutyCycle(2)
+            time.sleep(0.5)
+        elif distance > 17:
+            servo1.ChangeDutyCycle(3)
+            time.sleep(0.5)
+        elif distance > 15:
+            servo1.ChangeDutyCycle(4)
+            time.sleep(0.5)
+        elif distance > 10:
+            servo1.ChangeDutyCycle(6)
+            time.sleep(0.5)
+        elif distance > 5:
+            servo1.ChangeDutyCycle(8)
+            time.sleep(0.5)
+        else:
+            servo1.ChangeDutyCycle(10)
+            time.sleep(0.5)
+        
+except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program
+    servo1.ChangeDutyCycle(2) 
+    time.sleep(0.5)
+    servo1.ChangeDutyCycle(0)
+    print("Package Drop Success")
+    servo1.stop()
+    GPIO.cleanup()
